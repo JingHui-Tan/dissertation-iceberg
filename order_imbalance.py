e9a2bb9687d895994bc8d2d2c04dfc00c299cf73
JingHui-T <97815653+JingHui-T@users.noreply.github.com>
2024-08-03 14:24:22 +0800

    Add size conditional OI

--- order_imbalance.py
+++ order_imbalance.py
@@ -45,6 +45,8 @@ def iceberg_tag(df, ib_delta):
 def calculate_log_returns(grouped):
     grouped['log_ret'] = np.log(grouped['last_midprice']) - np.log(grouped['first_midprice'])
     grouped['fut_log_ret'] = grouped['log_ret'].shift(-1)
+    grouped['weighted_log_ret'] = np.log(grouped['last_weighted_mp']) - np.log(grouped['first_weighted_mp'])
+    grouped['fut_weighted_log_ret'] = grouped['weighted_log_ret'].shift(-1)
     return grouped
 
 
@@ -69,6 +71,9 @@ def calculate_order_imbalance(df, direction_column, size_column, pred_dir_column
 
 
 def order_imbalance(df_full, df_pred=None, df_ob=None, delta='30S', type='vis'):
+    weight = df_ob['bid_size_1'] / (df_ob['bid_size_1'] + df_ob['ask_size_1'])
+    df_full['weighted_mp'] = weight * df_ob['ask_price_1'] + (1 - weight) * df_ob['bid_price_1']
+
     if type == 'vis':
         df = df_full[df_full['event_type'] == 4]
     elif type == 'hid':
@@ -84,7 +89,9 @@ def order_imbalance(df_full, df_pred=None, df_ob=None, delta='30S', type='vis'):
         lambda x: pd.Series({
             'order_imbalance': calculate_order_imbalance(x, 'direction', 'size', 'pred_dir' if type == 'hid' else None),
             'first_midprice': x['midprice'].iloc[0],
-            'last_midprice': x['midprice'].iloc[-1]
+            'last_midprice': x['midprice'].iloc[-1],
+            'first_weighted_mp': x['weighted_mp'].iloc[0],
+            'last_weighted_mp': x['weighted_mp'].iloc[-1]
         })
     ).reset_index()
 
@@ -93,21 +100,26 @@ def order_imbalance(df_full, df_pred=None, df_ob=None, delta='30S', type='vis'):
 
     grouped = filter_quantiles(grouped, 'log_ret')
     grouped = filter_quantiles(grouped, 'fut_log_ret')
+    grouped = filter_quantiles(grouped, 'weighted_log_ret')
+    grouped = filter_quantiles(grouped, 'fut_weighted_log_ret')
+
     
     return grouped[:-1]
 
 
 
-def combined_order_imbalance(df_full, df_pred, delta='5min'):
-    df_vis = order_imbalance(df_full=df_full, delta=delta, type='vis')
-    df_hid = order_imbalance(df_full=df_full, df_pred=df_pred, delta=delta, type='hid')
+def combined_order_imbalance(df_full, df_pred, df_ob, delta='5min'):
+    df_vis = order_imbalance(df_full=df_full, df_ob=df_ob, delta=delta, type='vis')
+    df_hid = order_imbalance(df_full=df_full, df_ob=df_ob, df_pred=df_pred, delta=delta, type='hid')
 
     return df_vis.merge(df_hid[['datetime_bins', 'order_imbalance']], on='datetime_bins', suffixes=('_vis', '_hid'))
 
 
 
 
-def iceberg_order_imbalance(df_full, df_pred, delta='5min'):
+def iceberg_order_imbalance(df_full, df_pred, df_ob, delta='5min', weighted=False):
+    weight = df_ob['bid_size_1'] / (df_ob['bid_size_1'] + df_ob['ask_size_1'])
+    df_full['weighted_mp'] = weight * df_ob['ask_price_1'] + (1 - weight) * df_ob['bid_price_1']
     ib_delta = '1ms'
     df = iceberg_tag(df_full, ib_delta)
     df['datetime_bins'] = df.index.get_level_values('datetime').ceil(delta)
@@ -116,7 +128,9 @@ def iceberg_order_imbalance(df_full, df_pred, delta='5min'):
             'order_imbalance_vis': calculate_order_imbalance(x[x['iceberg'] == 0], 'direction', 'size'),
             'order_imbalance_ib': calculate_order_imbalance(x[x['iceberg'] == 1], 'direction', 'size'),
             'first_midprice': x['midprice'].iloc[0],
-            'last_midprice': x['midprice'].iloc[-1]
+            'last_midprice': x['midprice'].iloc[-1],
+            'first_weighted_mp': x['weighted_mp'].iloc[0],
+            'last_weighted_mp': x['weighted_mp'].iloc[-1]
         })
     ).reset_index()
 
@@ -126,8 +140,11 @@ def iceberg_order_imbalance(df_full, df_pred, delta='5min'):
 
     grouped = filter_quantiles(grouped, 'log_ret')
     grouped = filter_quantiles(grouped, 'fut_log_ret')
+    grouped = filter_quantiles(grouped, 'weighted_log_ret')
+    grouped = filter_quantiles(grouped, 'fut_weighted_log_ret')
 
-    df_hid = order_imbalance(df_full=df_full, df_pred=df_pred, delta=delta, type='hid')
+
+    df_hid = order_imbalance(df_full=df_full, df_pred=df_pred, df_ob=df_ob, delta=delta, type='hid')
     df_hid.rename(columns={"order_imbalance": "order_imbalance_hid"}, inplace=True)
 
     return grouped.merge(df_hid[['datetime_bins', 'order_imbalance_hid']], on='datetime_bins')
@@ -141,69 +158,103 @@ def agg_order(df, df_pred, agg='agg_low'):
         return df[((df_pred['pred_dir'] == 1) & (df['agg_ratio'] < 0.5)) | 
                   ((df_pred['pred_dir'] == -1) & (df['agg_ratio'] > 0.5))]
     
-    if agg=='agg_mid':
+    elif agg=='agg_mid':
         return df[df['agg_ratio'] == 0.5]
 
-    if agg=='agg_high':
+    elif agg=='agg_high':
         return df[((df_pred['pred_dir'] == 1) & (df['agg_ratio'] > 0.5)) | 
                   ((df_pred['pred_dir'] == -1) & (df['agg_ratio'] < 0.5))]
 
 
+def size_order(df, df_pred, size='small'):
+    df = df[df['event_type']==5]
+    lower_q = df['size'].quantile(1/3)
+    upper_q = df['size'].quantile(2/3)
+
+    if size=='small':
+        return df[df['size'] < lower_q]
+    
+    elif size=='medium':
+        return df[(df['size'] > lower_q) & (df['size'] < upper_q)]
+
+    elif size=='large':
+        return df[df['size'] > upper_q]
 
 
-def conditional_order_imbalance(df_full, df_pred, delta='5min', condition='agg'):
+
+def conditional_order_imbalance(df_full, df_pred, df_ob, delta='5min', condition='agg'):
+
+    df_fin = order_imbalance(df_full=df_full, delta=delta, df_ob=df_ob, type='vis')
+    df_fin.rename(columns={'order_imbalance': 'order_imbalance_vis'}, inplace=True)
+
     if condition == 'agg':
-        df_fin = order_imbalance(df_full=df_full, delta=delta, type='vis')
-        df_fin.rename(columns={'order_imbalance': 'order_imbalance_vis'}, inplace=True)
-        
         for version, suffix in zip(['agg_low', 'agg_mid', 'agg_high'], ['_agg_low', '_agg_mid', '_agg_high']):
-            df = order_imbalance(df_full=agg_order(df_full, df_pred, agg=version), df_pred=df_pred, delta=delta, type='hid')
+            df = order_imbalance(df_full=agg_order(df_full, df_pred, agg=version), df_pred=df_pred, df_ob=df_ob, delta=delta, type='hid')
             df.rename(columns={'order_imbalance': f'order_imbalance{suffix}'}, inplace=True)
             df_fin = df_fin.merge(df[['datetime_bins', f'order_imbalance{suffix}']], on='datetime_bins')
 
-        return df_fin
+    elif condition == 'size':
+        for version in ['small', 'medium', 'large']:
+            df = order_imbalance(df_full=size_order(df_full, df_pred, size=version), df_pred=df_pred, df_ob=df_ob, delta=delta, type='hid')
+            df.rename(columns={'order_imbalance': f'order_imbalance_{version}'}, inplace=True)
+            df_fin = df_fin.merge(df[['datetime_bins', f'order_imbalance_{version}']], on='datetime_bins')
+
+    return df_fin
 
 
 
-def lm_results(df_full, df_pred, delta_lst, order_type='combined', predictive=True):
+def lm_results(df_full, df_pred, df_ob, delta_lst, order_type='combined', predictive=True, weighted_mp=False,
+               momentum=False):
 
-    y = "fut_log_ret" if predictive else "log_ret"
-    x_momentum = "log_ret" if predictive else ""
+    if weighted_mp==False:
+        y = "fut_log_ret" if predictive else "log_ret"
+        x_momentum = "+ log_ret" if momentum else ""
+    else:
+        y = 'fut_weighted_log_ret' if predictive else "weighted_log_ret"
+        x_momentum = "+ weighted_log_ret" if momentum else ""
     
     params_lst = []
     tvalues_lst = []
 
     for delta in delta_lst:
         if order_type == 'vis' or order_type == 'hid' or order_type == 'combined':
-            df_merged = combined_order_imbalance(df_full, df_pred, delta=delta)
+            df_merged = combined_order_imbalance(df_full, df_pred, df_ob, delta=delta)
 
         elif order_type == 'comb_iceberg':
-            df_merged = iceberg_order_imbalance(df_full, df_pred, delta=delta)
+            df_merged = iceberg_order_imbalance(df_full, df_pred, df_ob, delta=delta)
             lm = smf.ols(formula=f"""{y} ~ order_imbalance_vis + order_imbalance_hid + order_imbalance_ib + {x_momentum}""", 
                          data=df_merged).fit()
             params_lst.append((lm.params[1], lm.params[2], lm.params[3]))
             tvalues_lst.append((lm.tvalues[1], lm.tvalues[2], lm.tvalues[3]))
 
         elif order_type == 'agg':
-            df_merged = conditional_order_imbalance(df_full, df_pred, delta=delta, condition='agg')
+            df_merged = conditional_order_imbalance(df_full, df_pred, df_ob, delta=delta, condition='agg')
             lm = smf.ols(formula=f"""{y} ~ order_imbalance_vis + order_imbalance_agg_low + \n
-                         order_imbalance_agg_mid + order_imbalance_agg_high + {x_momentum}""", 
+                         order_imbalance_agg_mid + order_imbalance_agg_high {x_momentum}""", 
+                         data=df_merged).fit()
+            params_lst.append((lm.params[1], lm.params[2], lm.params[3], lm.params[4]))
+            tvalues_lst.append((lm.tvalues[1], lm.tvalues[2], lm.tvalues[3], lm.tvalues[4]))
+        
+        elif order_type == 'size':
+            df_merged = conditional_order_imbalance(df_full, df_pred, df_ob, delta=delta, condition='size')
+            lm = smf.ols(formula=f"""{y} ~ order_imbalance_vis + order_imbalance_small + \n
+                         order_imbalance_medium + order_imbalance_large {x_momentum}""", 
                          data=df_merged).fit()
             params_lst.append((lm.params[1], lm.params[2], lm.params[3], lm.params[4]))
             tvalues_lst.append((lm.tvalues[1], lm.tvalues[2], lm.tvalues[3], lm.tvalues[4]))
 
         if order_type == "vis":
-            lm = smf.ols(formula=f"""{y} ~ order_imbalance_vis + {x_momentum}""", data=df_merged).fit()
+            lm = smf.ols(formula=f"""{y} ~ order_imbalance_vis {x_momentum}""", data=df_merged).fit()
             params_lst.append(lm.params[1])
             tvalues_lst.append(lm.tvalues[1])
 
         elif order_type == "hid":
-            lm = smf.ols(formula=f"""{y} ~ order_imbalance_hid + {x_momentum}""", data=df_merged).fit()
+            lm = smf.ols(formula=f"""{y} ~ order_imbalance_hid {x_momentum}""", data=df_merged).fit()
             params_lst.append(lm.params[1])
             tvalues_lst.append(lm.tvalues[1])
 
         elif order_type == "combined":
-            lm = smf.ols(formula=f"""{y} ~ order_imbalance_vis + order_imbalance_hid + {x_momentum}""", data=df_merged).fit()
+            lm = smf.ols(formula=f"""{y} ~ order_imbalance_vis + order_imbalance_hid {x_momentum}""", data=df_merged).fit()
             params_lst.append((lm.params[1], lm.params[2]))
             tvalues_lst.append((lm.tvalues[1], lm.tvalues[2]))
 
@@ -248,7 +299,20 @@ def lm_results(df_full, df_pred, delta_lst, order_type='combined', predictive=Tr
             'tvalues_agg_high': [x[3] for x in tvalues_lst],
         })
         return df
-
+    
+    elif order_type == 'size':
+        df = pd.DataFrame({
+            'timeframe': delta_lst,
+            'params_vis': [x[0] for x in params_lst],
+            'tvalues_vis': [x[0] for x in tvalues_lst],
+            'params_small': [x[1] for x in params_lst],
+            'tvalues_small': [x[1] for x in tvalues_lst],
+            'params_medium': [x[2] for x in params_lst],
+            'tvalues_medium': [x[2] for x in tvalues_lst],
+            'params_large': [x[3] for x in params_lst],
+            'tvalues_large': [x[3] for x in tvalues_lst],
+        })
+        return df
 
 
 def diagnostic_plots(lm, y, data):
